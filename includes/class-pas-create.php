8d1fce78a64cf818e3739b36c850befd7f2ca3d0
aleksioz <aleksa.mejl@gmail.com>
2025-02-01 11:56:30 +0100

    Fix category detection

--- includes/class-pas-create.php
+++ includes/class-pas-create.php
@@ -20,7 +20,8 @@ class PasCreate {
     public function connect_data_on_submit( $form_id, $response ) { 
         $entry = forminator_get_latest_entry_by_form_id( $form_id );
         $post_id = sanitize_text_field( $entry->meta_data['postdata-1']['value']['postdata'] );
-        $cat_id = sanitize_text_field($entry->meta_data['select-4']['value']);
+        $cat = sanitize_text_field($entry->meta_data['select-4']['value']); // Izgubljeni / Pronađeni i vidjeni
+        
         if ( ! $post_id ) {
             $post_id = wp_insert_post(array(
                 'post_title' => "Pronadjen " . date('d.m.Y'),
@@ -36,14 +37,14 @@ class PasCreate {
             $this->make_table( $entry ) 
         );
     
-        $post_content .= $this->make_text( $entry, $cat_id );
+        $post_content .= $this->make_text( $entry, $cat );
     
         $this->update_postmeta_and_thumb( $entry, $post_id );
     
         wp_update_post(array(
             'ID'           => $post_id,
             'post_content' => $post_content,
-            'post_category' => array( $cat_id )
+            'post_category' => array( get_cat_ID( $cat ) ),
         ));
     
     }
@@ -95,12 +96,12 @@ class PasCreate {
         return  $table;
     }
 
-    private function make_text( $entry, $cat_id ){
+    private function make_text( $entry, $cat ){
         $content = '';
     
         $rows = [
             'Osobenost' => sanitize_text_field($entry->meta_data['textarea-1']['value']),
-            'Ime vlasnika' => $cat_id==1 ? sanitize_text_field($entry->meta_data['name-1']['value']) : sanitize_text_field($entry->meta_data['name-2']['value']),
+            'Ime vlasnika' => $cat=="Izgubljeni" ? sanitize_text_field($entry->meta_data['name-1']['value']) : sanitize_text_field($entry->meta_data['name-2']['value']),
             'Telefon' => sanitize_text_field($entry->meta_data['phone-1']['value']),
             'Email' => sanitize_text_field($entry->meta_data['email-1']['value']),
         ];
@@ -114,7 +115,7 @@ class PasCreate {
         $content .= '<!-- wp:separator --><hr class="wp-block-separator has-alpha-channel-opacity"/><!-- /wp:separator -->';
     
         $content .= '<!-- wp:heading {"level":6} --><h6 class="wp-block-heading">'; 
-            $cat_id == 1 ? $content .= 'vlasnik' : $content .= 'pronalazač';
+            $cat == "Izgubljeni" ? $content .= 'vlasnik' : $content .= 'pronalazač';
         $content .= '</h6><!-- /wp:heading -->';
     
         $content .= '<!-- wp:paragraph --><p>' . $rows['Ime vlasnika'] . '<br>' . $rows['Telefon'] . '<br>' . $rows['Email']  . '</p><!-- /wp:paragraph -->';
